414eb5d796430da7787537481ffa2fe04d7c25af
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-15 20:26:02 +0500

    test: server disconnected receiver called when scene tree signal emitted

--- test/unit/test_lobby.gd
+++ test/unit/test_lobby.gd
@@ -1,20 +1,43 @@
 extends GutTest
 
+
+class Utils:
+	static func make_lobby(tst_inst):
+		var lobby = tst_inst.partial_double("res://scripts/lobby.gd", tst_inst.DOUBLE_STRATEGY.FULL).new()
+		tst_inst.stub(lobby, "_ready").to_call_super()
+		return lobby
+
+
+class TestInSceneTree:
+	extends GutTest
+
+	var lobby
+
+	func before_each():
+		lobby = Utils.make_lobby(self)
+
+	func test_connected_to_server_receiver_called_when_scene_tree_signal_emitted():
+		stub(lobby, "_connected_to_server")
+		add_child(lobby)
+
+		get_tree().emit_signal("connected_to_server")
+
+		assert_called(lobby, "_connected_to_server")
+
+	func test_server_disconnected_receiver_called_when_scene_tree_signal_emitted():
+		stub(lobby, "_server_disconnected")
+		add_child(lobby)
+
+		get_tree().emit_signal("server_disconnected")
+
+		assert_called(lobby, "_server_disconnected")
+
+
 var lobby
 
 
 func before_each():
-	lobby = partial_double("res://scripts/lobby.gd", DOUBLE_STRATEGY.FULL).new()
-
-
-func test_connected_to_server_receiver_called_when_scene_tree_signal_emitted():
-	stub(lobby, "_ready").to_call_super()
-	stub(lobby, "_connected_to_server")
-	add_child(lobby)
-
-	get_tree().emit_signal("connected_to_server")
-
-	assert_called(lobby, "_connected_to_server")
+	lobby = Utils.make_lobby(self)
 
 
 func test_create_server_calls_set_network_peer_on_scene_tree():
