b481efb3644cf4c698fc765e3898d38e13cd4582
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-23 08:22:02 +0500

    test: connecting to server properly initializes info on all peers

---
+++ test/integration/test_integ_lobby.gd
@@ -0,0 +1,63 @@
+extends GutTest
+
+
+func make_lobby_in_env_from_info(info):
+	var lobby = autofree(Lobby.new())
+	lobby.info = info
+	lobby.name = "Lobby"
+	var sub_st = add_child_autofree(SubSceneTree.new())
+	sub_st.st.root.add_child(lobby)
+	return lobby
+
+
+func before_all():
+	Logger.set_logger_level(Logger.LOG_LEVEL_WARN)
+
+
+func test_connecting_to_server_properly_initializes_info_on_all_peers():
+	var infos = {
+		"server": Lobby.LobbyUtils.make_info_with_name("Server"),
+		"client1": Lobby.LobbyUtils.make_info_with_name("Client 1"),
+		"client2": Lobby.LobbyUtils.make_info_with_name("Client 2")
+	}
+
+	var server_lobby = make_lobby_in_env_from_info(infos.server)
+	var client_1_lobby = make_lobby_in_env_from_info(infos.client1)
+	var client_2_lobby = make_lobby_in_env_from_info(infos.client2)
+
+	server_lobby.create_server()
+	client_1_lobby.join_server()
+	client_2_lobby.join_server()
+
+	yield(yield_for(0.5), YIELD)
+
+	var expected_players_info = [
+		Lobby.LobbyUtils.make_player_info_with_id(
+			server_lobby.get_tree().get_network_unique_id(), infos.server
+		),
+		Lobby.LobbyUtils.make_player_info_with_id(
+			client_1_lobby.get_tree().get_network_unique_id(), infos.client1
+		),
+		Lobby.LobbyUtils.make_player_info_with_id(
+			client_2_lobby.get_tree().get_network_unique_id(), infos.client2
+		),
+	]
+
+	assert_true(
+		TestUtils.is_array_similar(
+			server_lobby.players_info, expected_players_info
+		),
+		"check server players_info is updated"
+	)
+	assert_true(
+		TestUtils.is_array_similar(
+			client_1_lobby.players_info, expected_players_info
+		),
+		"check client 1 players_info is updated"
+	)
+	assert_true(
+		TestUtils.is_array_similar(
+			client_2_lobby.players_info, expected_players_info
+		),
+		"check client 2 players_info is updated"
+	)
