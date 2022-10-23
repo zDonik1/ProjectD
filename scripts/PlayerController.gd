fd6d75951f0e8b1e01288d6b001d58ed7b8d3a0c
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-23 20:15:58 +0500

    feat: organized main scene into main menu and game

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -3,14 +3,14 @@ extends Node
 onready var possessed = get_node("/root/Main/Player")
 
 func _ready():
-    pass
-    
+	pass
+	
 func  _process(delta):
-    possessed.set_move_input(calc_move_input())
+	possessed.set_move_input(calc_move_input())
 
 func _input(event):
-    pass
-    
+	pass
+	
 func calc_move_input():
-    return Vector2(Input.get_axis("ui_left", "ui_right"),
-                   Input.get_axis("ui_up", "ui_down"))
+	return Vector2(Input.get_axis("ui_left", "ui_right"),
+				   Input.get_axis("ui_up", "ui_down"))
