84f567ec9cdd93683bc0444de6fbee2ea2ea9675
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-23 19:02:50 +0500

    feat: log dir and console logger disabling

--- scripts/initial_setup.gd
+++ scripts/initial_setup.gd
@@ -7,5 +7,6 @@ func _init():
 	if OS.has_environment("PROJECT_D_LOG_FORMAT"):
 		Logger.set_logger_format(Logger._get_logger_format_by_name(OS.get_environment("PROJECT_D_LOG_FORMAT")))
 		
-	if OS.has_environment("PROJECT_D_ENABLE_FILE_LOGGER"):
-		Logger.add_appender(FileAppender.new())
+	if OS.has_environment("PROJECT_D_ONLY_FILE_LOGGER"):
+		Logger.logger_appenders.clear()
+		Logger.add_appender(FileAppender.new("user://logs/log.txt"))
