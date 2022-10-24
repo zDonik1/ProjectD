c6a83b23fa415e24159b0963ca757d255068695f
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-24 08:06:22 +0500

    feat: pressing create server button deletes main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,3 +11,5 @@ func _create_server_button_pressed():
 	var lobby = preload("res://scenes/lobby.tscn").instance()
 	lobby.name = "LobbyUI"
 	get_parent().add_child(lobby)
+
+	queue_free()
--- test/unit/test_main_menu.gd
+++ test/unit/test_main_menu.gd
@@ -2,16 +2,19 @@ extends UnitTest
 
 var main_menu
 
+
 func before_each():
 	.before_each()
 	main_menu = partial_double("res://scenes/main_menu.tscn").instance()
 	stub(main_menu, "_ready").to_call_super()
 	add_child(main_menu)
 
+func after_each():
+	if has_node("LobbyUI"):
+		$LobbyUI.free()
+
 
 func test_connects_create_server_button_pressed_to_receiver():
-	stub(main_menu, "_create_server_button_pressed").to_do_nothing()
-	
 	main_menu.get_node("ButtonList/CreateServer").emit_signal("pressed")
 
 	assert_called(main_menu, "_create_server_button_pressed")
@@ -25,4 +28,11 @@ func test_pressing_create_server_button_creates_lobby():
 		"check that LobbyUI node was created"
 	)
 
-	$LobbyUI.free()
+
+func test_pressing_create_server_button_deletes_main_menu():
+	main_menu._create_server_button_pressed()
+
+	assert_true(
+		main_menu.is_queued_for_deletion(),
+		"check that MainMenu is about to be deleted"
+	)
