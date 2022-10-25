4cd97f1a0c5c6a028d32d355047112894abf83fe
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 07:39:58 +0500

    feat:  add_player receiver connected to lobby peer_added on ready

--- scripts/lobby_ui.gd
+++ scripts/lobby_ui.gd
@@ -1,6 +1,13 @@
 extends ItemList
 
 
+var lobby: Lobby
+
+
+func _ready():
+	var _u = lobby.connect("peer_added", self, "_add_player")
+
+
 func get_item_names():
 	var result = []
 	for i in range(get_item_count()):
@@ -12,3 +19,7 @@ func set_item_names(names):
 	clear()
 	for name in names:
 		add_item(name)
+
+
+func _add_player(info):
+	pass
--- test/unit/test_lobby_ui.gd
+++ test/unit/test_lobby_ui.gd
@@ -1,14 +1,30 @@
 extends UnitTest
 
+var lobby_ui
+
+
+func before_each():
+	.before_each()
+	lobby_ui = partial_double("res://scenes/lobby_ui.tscn").instance()
+	stub(lobby_ui, "_ready").to_call_super()
+
 
 func test_setting_names_to_list_with_existing_items_overrides_it():
 	var names = [
 		"My first item", "This is a second item", "Here is a third one"
 	]
-	var lobby_ui = autofree(preload("res://scenes/lobby_ui.tscn").instance())
 	lobby_ui.add_item("Some other item")
 
 	lobby_ui.set_item_names(names)
 
 	assert_eq(lobby_ui.get_item_names(), names)
-	
+
+
+func test_add_player_receiver_connected_to_lobby_peer_added_on_ready():
+	var lobby = autofree(Lobby.new())
+	lobby_ui.lobby = lobby
+	add_child(lobby_ui)
+
+	lobby.emit_signal("peer_added", TestUtils.get_player_info())
+
+	assert_called(lobby_ui, "_add_player", [TestUtils.get_player_info()])
