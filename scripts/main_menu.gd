327f2662b6c741ffd81594019deb42bd44bfeb04
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 16:04:07 +0500

    fix: removed lobby creation from main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,33 +4,21 @@ signal create_server_pressed
 signal join_server_pressed
 
 var coordinator: Coordinator
-var lobby: Lobby
 
 
 func _ready():
-	var _u
+	var _u: int
 	_u = $ButtonList/CreateServer.connect(
-		"pressed", self, "_create_server_button_pressed"
+		"pressed", self, "_on_create_server_button_pressed"
 	)
-
 	_u = $ButtonList/JoinServer.connect(
-		"pressed", self, "_join_server_button_pressed"
+		"pressed", self, "_on_join_server_button_pressed"
 	)
 
 
-func _create_server_button_pressed():
-	var lobby_ui = preload("res://scenes/lobby_ui.tscn").instance()
-	lobby_ui.name = "LobbyUI"
-	lobby_ui.lobby = lobby
-	get_parent().add_child(lobby_ui)
-
-	lobby.create_server()
+func _on_create_server_button_pressed():
+	emit_signal("create_server_pressed")
 
 
-func _join_server_button_pressed():
-	var message = preload("res://scenes/screen_message.tscn").instance()
-	message.name = "ConnectingMessage"
-	message.message = "Connecting to server..."
-	get_parent().add_child(message)
-
-	lobby.join_server()
+func _on_join_server_button_pressed():
+	emit_signal("join_server_pressed")
