8ab24a1afb8c7623907bd4099c3edb1907dd3192
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 14:01:10 +0500

    refactor(test_coordinator): split tests

--- test/unit/test_coordinator.gd
+++ test/unit/test_coordinator.gd
@@ -1,10 +1,20 @@
 extends UnitTest
 
+var coordinator: Node
+
+
+func before_each():
+	coordinator = add_child_autofree(Coordinator.new())
+
+
 func after_each():
-	get_node("MainMenu").free()
+	if has_node("MainMenu"):
+		get_node("MainMenu").free()
 
-func test_initializes_main_menu_as_sibling_on_ready():
-	var coordinator: Node = add_child_autofree(Coordinator.new())
 
+func test_creates_main_menu_as_sibling_on_ready():
 	assert_true(has_node("MainMenu"), "check MainMenu was created")
+
+
+func test_initializes_main_menu_coordinator_with_self_on_ready():
 	assert_eq(get_node("MainMenu").coordinator, coordinator)
