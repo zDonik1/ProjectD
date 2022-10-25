6871369e8a010873d64ae0212124ac1c01d43e89
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 13:20:33 +0500

    temp

--- test/utils.gd
+++ test/utils.gd
@@ -5,9 +5,9 @@ static func get_player_info():
 	return Lobby.LobbyUtils.make_info_with_name("Some player name")
 
 
-static func is_array_similar(actual_arr, expected_arr):
+static func is_array_similar(actual_arr: Array, expected_arr: Array) -> bool:
 	for elem in expected_arr:
-		var found = false
+		var found := false
 		for act_elem in actual_arr:
 			if elem.hash() == act_elem.hash():
 				found = true
