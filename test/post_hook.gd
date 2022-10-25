e118a9a4d415fe59c4e540d851af8de8c61a0692
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 18:01:13 +0500

    feat: failing tests on tests errors or warnings

--- test/post_hook.gd
+++ test/post_hook.gd
@@ -2,9 +2,17 @@ extends GutHookScript
 
 func run():
 	if gut.get_summary().get_totals().risky > 0:
-		Logger.error("Risky tests found")
+		Logger.error("Risky tests found", "TEST_CHECK")
 		set_exit_code(1)
 	
 	if gut.get_orphan_counter().get_counter("total") > 0:
-		Logger.error("Orphans created during test")
+		Logger.error("Orphans created during test", "TEST_CHECK")
+		set_exit_code(1)
+
+	if gut.get_logger().get_warnings().size() > 0:
+		Logger.error("Warnings issued", "TEST_CHECK")
+		set_exit_code(1)
+
+	if gut.get_logger().get_errors().size() > 0:
+		Logger.error("Erors issued", "TEST_CHECK")
 		set_exit_code(1)
