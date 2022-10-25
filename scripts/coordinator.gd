be11605f859f006d805731985fafce3f3c6edf9d
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 15:43:17 +0500

    feat: initializes lobby_ui with lobby

--- scripts/coordinator.gd
+++ scripts/coordinator.gd
@@ -1,11 +1,11 @@
 class_name Coordinator
 extends Node
 
+
 func _ready():
 	var main_menu: Node = load("res://scenes/main_menu.tscn").instance()
 	main_menu.name = "MainMenu"
 	main_menu.coordinator = self
-
 	var _u := main_menu.connect("create_server_pressed", self, "_add_lobby_ui")
 	get_parent().add_child(main_menu)
 
@@ -13,4 +13,5 @@ func _ready():
 func _add_lobby_ui():
 	var lobby_ui: Node = load("res://scenes/lobby_ui.tscn").instance()
 	lobby_ui.name = "LobbyUI"
+	lobby_ui.lobby = get_node("../Lobby")
 	get_parent().add_child(lobby_ui)
--- test/unit/test_coordinator.gd
+++ test/unit/test_coordinator.gd
@@ -1,29 +1,48 @@
-extends UnitTest
-
-var coordinator: Node
+extends GutTest
 
 
-func before_each():
-	coordinator = add_child_autofree(Coordinator.new())
+class CoordinatorEnv:
+	extends UnitTest
+
+	var coordinator: Node
+
+	func before_each():
+		coordinator = add_child_autofree(Coordinator.new())
+
+	func after_each():
+		if has_node("MainMenu"):
+			get_node("MainMenu").free()
+
+		if has_node("LobbyUI"):
+			get_node("LobbyUI").free()
 
 
-func after_each():
-	if has_node("MainMenu"):
-		get_node("MainMenu").free()
+class TestCoordinator:
+	extends CoordinatorEnv
 
-	if has_node("LobbyUI"):
-		get_node("LobbyUI").free()
+	func test_creates_main_menu_as_sibling_on_ready():
+		assert_true(has_node("MainMenu"), "check MainMenu was created")
 
-
-func test_creates_main_menu_as_sibling_on_ready():
-	assert_true(has_node("MainMenu"), "check MainMenu was created")
+	func test_initializes_main_menu_coordinator_with_self_on_ready():
+		assert_eq(get_node("MainMenu").coordinator, coordinator)
 
 
-func test_initializes_main_menu_coordinator_with_self_on_ready():
-	assert_eq(get_node("MainMenu").coordinator, coordinator)
+class TestCoordinatorWithLobbyUI:
+	extends CoordinatorEnv
 
+	var lobby: Node
 
-func test_creates_lobby_ui_as_sibling_when_main_menu_create_server_pressed_emits():
-	get_node("MainMenu").emit_signal("create_server_pressed")
-	
-	assert_true(has_node("LobbyUI"), "check LobbyUI was created")
+	func before_each():
+		.before_each()
+		lobby = add_child_autofree(Lobby.new())
+		lobby.name = "Lobby"
+
+	func test_creates_lobby_ui_as_sibling_when_main_menu_create_server_pressed_emits():
+		get_node("MainMenu").emit_signal("create_server_pressed")
+
+		assert_true(has_node("LobbyUI"), "check LobbyUI was created")
+
+	func test_initializes_lobby_ui_with_lobby():
+		get_node("MainMenu").emit_signal("create_server_pressed")
+
+		assert_eq(get_node("LobbyUI").lobby, lobby)
