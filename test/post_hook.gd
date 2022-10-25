04a7b956cb67cf9e5173246ba1e7460870d09637
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-10-25 13:44:35 +0500

    feat: checking for orphans and failing tests

--- test/post_hook.gd
+++ test/post_hook.gd
@@ -4,3 +4,7 @@ func run():
 	if gut.get_summary().get_totals().risky > 0:
 		Logger.error("Risky tests found")
 		set_exit_code(1)
+	
+	if gut.get_orphan_counter().get_counter("total") > 0:
+		Logger.error("Orphans created during test")
+		set_exit_code(1)
