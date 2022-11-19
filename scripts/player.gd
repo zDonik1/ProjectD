12e6f363190c965f956e9f8b9902fa04c75a5cfe
zDonik1 <tokhirovdoniyor@gmail.com>
2022-11-19 16:43:35 +0500

    feat: player with networked movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,31 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export(int) var max_speed = 200
+var max_speed := 200
+var position_update_rate := 0.03
+var spawn_position := Vector2.ZERO
+
+var _pos_update_time_accumulate := 0.0
+var _move_input := Vector2.ZERO
 
 
-func set_move_input(move_input):
-	linear_velocity = move_input * max_speed
+func _ready():
+	if is_network_master():
+		position = spawn_position
+
+
+func _physics_process(delta: float):
+	var _u := move_and_slide(_move_input * max_speed)
+
+	if is_network_master():
+		_pos_update_time_accumulate += delta
+		if _pos_update_time_accumulate >= position_update_rate:
+			_pos_update_time_accumulate = 0
+			rpc_unreliable("_update_position", position)
+
+
+func set_move_input(move_input: Vector2):
+	_move_input = move_input
+
+
+remote func _update_position(_position: Vector2):
+	position = _position
