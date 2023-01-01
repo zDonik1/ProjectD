ac0155f262fa5e14d65326397ad1ed72b65cd7d1
zDonik1 <tokhirovdoniyor@gmail.com>
2023-01-01 11:29:38 +0500

    fix: removed player name from main menu screen

--- scripts/main_menu_screen.gd
+++ scripts/main_menu_screen.gd
@@ -3,16 +3,6 @@ extends Control
 signal host_game_pressed
 signal join_server_pressed
 
-var player_name: String setget set_player_name, get_player_name
-
-
-func set_player_name(_name: String):
-	$VBoxContainer/PlayerName.text = _name
-
-
-func get_player_name():
-	return $VBoxContainer/PlayerName.text
-
 
 func _on_HostGame_pressed():
 	emit_signal("host_game_pressed")
